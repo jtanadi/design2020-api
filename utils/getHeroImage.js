8ceaad15bff7fc3315faaff8536a94116bd1983d
Jesen Tanadi <mail@jesentanadi.com>
2020-05-05 08:02:13 -0400

    rename dirs

--- utils/getHeroImage.js
+++ utils/getHeroImage.js
@@ -3,12 +3,12 @@ const path = require("path")
 
 // Hero image defaults to first image
 module.exports = (projectTitle) => {
-  const imageDir = path.join(process.cwd(), "_works", projectTitle);
+  const imageDir = path.join(process.cwd(), "works", projectTitle);
 
   const files = fs.readdirSync(imageDir);
   const heroImg = files.find((file) =>
     /\.(jpg$)|(jpeg$)|(png$)|(gif$)/.test(file)
   );
 
-  return path.join("/_works", projectTitle, heroImg);
+  return path.join("/works", projectTitle, heroImg);
 };
