b9472e23546499c78b7b205d97716d7251395bcd
Jesen Tanadi <mail@jesentanadi.com>
2020-05-05 21:36:54 -0400

    move checkers to own module

--- utils/getHeroImage.js
+++ utils/getHeroImage.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 const path = require("path");
 
+const { isImage } = require("./checkers");
+
 // Hero image defaults to first image
 module.exports = (projectTitle) => {
   const imageDir = path.join(process.cwd(), "works", projectTitle);
 
   const files = fs.readdirSync(imageDir);
-  const heroImg = files.find((file) =>
-    /\.(jpg|jpeg|png|gif|tif|tiff)$/.test(file)
-  );
+  const heroImg = files.find((file) => isImage(file));
 
   return path.join("/works", projectTitle, heroImg);
 };
--- utils/getImages.js
+++ utils/getImages.js
@@ -1,26 +1,22 @@
 const fs = require("fs");
 const path = require("path");
 
-const isHoverFile = (filename) => {
-  return /_over\.(jpg|jpeg|png|gif|tif|tiff)$/.test(filename);
-};
+const { isDetailFile, isImage } = require("./checkers");
 
 module.exports = (projectTitle) => {
   const imageDir = path.join(process.cwd(), "works", projectTitle);
 
   const files = fs.readdirSync(imageDir);
-  const imageFiles = files.filter((file) =>
-    /\.(jpg|jpeg|png|gif|tif|tiff)$/.test(file)
-  );
+  const imageFiles = files.filter((file) => isImage(file));
 
   return imageFiles.reduce((acc, imageFile, i) => {
-    if (!isHoverFile(imageFile)) {
+    if (!isDetailFile(imageFile)) {
       const retObj = {
         url: path.join("/works", projectTitle, imageFile),
       };
 
       const nextImage = imageFiles[i + 1];
-      if (nextImage && isHoverFile(nextImage)) {
+      if (nextImage && isDetailFile(nextImage)) {
         retObj.hoverUrl = path.join("/works", projectTitle, nextImage);
       }
 
