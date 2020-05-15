8a59fd713458040d8a12a55ec3a08e4c79884229
Jesen Tanadi <mail@jesentanadi.com>
2020-05-15 07:19:59 -0400

    exclude z- folders

--- utils/getWorkIds.js
+++ utils/getWorkIds.js
@@ -3,11 +3,13 @@ const { worksPath } = require("./paths");
 
 module.exports = () => {
   const works = fs.readdirSync(worksPath);
-  return works.map((work) => {
-    return {
-      params: {
-        id: work,
-      },
-    };
-  });
+  return works
+    .filter((work) => !work.startsWith("z-"))
+    .map((work) => {
+      return {
+        params: {
+          id: work,
+        },
+      };
+    });
 };
